bba746d1c1ce20aa2d34f11b133c09190365029d
Shreya Kurian <shreyakurian@Shreyas-MacBook-Pro.local>
2021-10-08 21:31:16 +0530

    Modified sqlite_custom_regexp_function

--- config/initializers/sqlite_custom_regexp_function.rb
+++ config/initializers/sqlite_custom_regexp_function.rb
@@ -6,7 +6,9 @@ ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
   def initialize(connection, logger = nil, pool = nil)
     orig_initialize(connection, logger, pool)
 
-    if connection.is_a? SQLite3::Database
+    is_sqlite_db = ActiveRecord::Base.connection_db_config.configuration_hash[:adapter] == "sqlite3"
+
+    if is_sqlite_db
       connection.create_function("regexp", 2) do |fn, pattern, expr|
         regex_matcher = Regexp.new(pattern.to_s, Regexp::IGNORECASE)
         fn.result = expr.to_s.match(regex_matcher) ? 1 : 0
